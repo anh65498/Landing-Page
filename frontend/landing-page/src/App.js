71cc6b9273764e6e98b6c2422464dc4b3237ddd4
Anh <beepboop@Anhs-MacBook-Pro.local>
2020-08-08 05:10:20 -0700

    Working on landing page

---
+++ frontend/landing-page/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import NavBar from "./components/NavBar";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <Fragment>
+      <NavBar />
+      <LandingPage />
+    </Fragment>
+  );
+}
+
+export default App;
